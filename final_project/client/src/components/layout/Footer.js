835894182771f1feacda026140696d17dcafacc1
vivek mannava <vivek@viveks-MacBook-Air.local>
2022-11-29 18:41:59 -0500

    second vivek

---
+++ final_project/client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+ 
+const Footer = () => {
+  return (
+       <footer className="py-1">
+      <p className="text-center mt-1" style={{color:"#2872F0"}}>
+        <b>Go Shop, All Rights Reserved</b>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
