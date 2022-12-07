3a2fa57dac56749a2d0537760a75d4ed11cba09d
steja108 <113944211+steja108@users.noreply.github.com>
2022-12-07 18:54:48 -0500

    Jan

--- final_project/client/src/components/layout/Footer.js
+++ final_project/client/src/components/layout/Footer.js
@@ -1,11 +1,79 @@
 import React from 'react'
+import './Footer.css';
+
  
 const Footer = () => {
+  const back = {backgroundImage:'url(https://res.cloudinary.com/djqty7gls/image/upload/v1670449775/shape-11_yjib92.png)'};
+  const width = {width:'40px'};
   return (
-       <footer className="py-1">
-      <p className="text-center mt-1" style={{color:"#2872F0"}}>
-        <b>Go Shop, All Rights Reserved</b>
-      </p>
+    <footer class="main-footer bg-color-2">
+      <div class="footer-top pt-130">
+      <div class="vector-bg" style={{...back}}></div>
+                <div class="auto-container">
+                    <div class="row clearfix">
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget logo-widget">
+                                <figure class="footer-logo"><img src="https://res.cloudinary.com/djqty7gls/image/upload/v1670449285/logo-new_lco1yc.png" alt="" style={{...width}}></img></figure>
+                                <div class="text">
+                                    <p>Lorem ipsum dolor amet consetetur adi pisicing elit sed eiusm tempor in cididunt ut labore dolore magna aliqua enim.</p>
+                                </div>
+                                <ul class="social-links clearfix">
+                                    <li><i class="fab fa-facebook-f"></i></li>
+                                    <li><i class="fab fa-twitter"></i></li>
+                                    <li><i class="fab fa-instagram"></i></li>
+                                    <li><i class="fab fa-google-plus-g"></i></li>
+                                    <li><i class="fab fa-linkedin-in"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget links-widget">
+                                <div class="widget-title">
+                                    <h3>Services</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <ul class="links-list clearfix">
+                                        <li>About Us</li>
+                                        <li>Listing</li>
+                                        <li>How It Works</li>
+                                        <li>Our Services</li>
+                                        <li>Our Blog</li>
+                                        <li>Contact Us</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget contact-widget">
+                                <div class="widget-title">
+                                    <h3>Contacts</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <ul class="info-list clearfix">
+                                        <li><i class="fas fa-map-marker-alt"></i>Flat 20, Reynolds Neck, North Helenaville, FV77 8WS</li>
+                                        <li><i class="fas fa-microphone"></i><a href="tel:23055873407">+2(305) 587-3407</a></li>
+                                        <li><i class="fas fa-envelope"></i><a href="mailto:info@example.com">info@example.com</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="auto-container">
+                    <div class="bottom-inner clearfix">
+                        <div class="copyright pull-left">
+                            <p><a href="index.html">Go Shop</a> &copy; 2022 All Right Reserved</p>
+                        </div>
+                        <ul class="footer-nav pull-right">
+                            <li><a href="index.html">Terms of Service</a></li>
+                            <li><a href="index.html">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+      </div>
+
     </footer>
   )
 }
