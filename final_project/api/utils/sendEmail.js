93fcd7b487274c66c2ec613467cabc799f5b6915
steja108 <113944211+steja108@users.noreply.github.com>
2022-12-08 12:33:33 -0500

    Rishik

--- final_project/api/utils/sendEmail.js
+++ final_project/api/utils/sendEmail.js
@@ -3,6 +3,8 @@ const nodemailer = require('nodemailer')
 const sendEmail = async options => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure:true,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD
