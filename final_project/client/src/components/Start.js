6e80dc22920e1800619adf6bc5f62a25e563a033
steja108 <113944211+steja108@users.noreply.github.com>
2022-12-08 16:18:24 -0500

    Saiteja

--- final_project/client/src/components/Start.js
+++ final_project/client/src/components/Start.js
@@ -11,26 +11,36 @@ const Start = () => {
     return(
         <div className='caro'>
         <h1 align="center"><b>Go shop</b></h1>
-        <Carousel autoPlay interval="500" transitionTime="5000" infiniteLoop>
+        <Carousel autoPlay interval="500" transitionTime="1400" infiniteLoop>
             <div>
                 <img src="https://res.cloudinary.com/djqty7gls/image/upload/v1670477429/electronics_lpxyue.jpg" alt="" />
+                <Link to='/home'>
                 <p className="legend">Electronics</p>
+                </Link>
             </div>
             <div>
                 <img src="https://res.cloudinary.com/djqty7gls/image/upload/v1670477908/food_hcqsii.jpg" alt="" />
+                <Link to='/home'>
                 <p className="legend">Food</p>
+                </Link>
             </div>
             <div>
                 <img src="https://res.cloudinary.com/djqty7gls/image/upload/v1670478014/clothes_otybmi.jpg" alt="" />
+                <Link to='/home'>
                 <p className="legend">Clothes</p>
+                </Link>
             </div>
             <div>
                 <img src="https://res.cloudinary.com/djqty7gls/image/upload/v1670478394/beauty_htxfip.jpg" alt="" />
+                <Link to='/home'>
                 <p className="legend">Beauty Products</p>
+                </Link>
             </div>
             <div>
                 <img src="https://res.cloudinary.com/djqty7gls/image/upload/v1670478595/books_gtnxfg.jpg" alt="" />
+                <Link to='/home'>
                 <p className="legend">Books</p>
+                </Link>
             </div>
         </Carousel>
         <div align="center" className='butt'>
--- final_project/client/src/components/layout/Footer.js
+++ final_project/client/src/components/layout/Footer.js
@@ -6,37 +6,35 @@ const Footer = () => {
   const back = {backgroundImage:'url(https://res.cloudinary.com/djqty7gls/image/upload/v1670449775/shape-11_yjib92.png)'};
   const width = {width:'40px'};
   return (
-    <footer class="main-footer bg-dark">
+    <footer className="main-footer bg-dark">
       <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
       <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
       <script src="https://kit.fontawesome.com/95817e6879.js" crossorigin="anonymous"></script>
-      <div class="footer-top pt-130">
-      <div class="vector-bg" style={{...back}}></div>
-                <div class="auto-container">
-                    <div class="row clearfix">
-                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                            <div class="footer-widget logo-widget">
-                                <figure class="footer-logo"><img src="https://res.cloudinary.com/djqty7gls/image/upload/v1670449285/logo-new_lco1yc.png" alt="" style={{...width}}></img></figure>
-                                <div class="text">
-                                    <p>Lorem ipsum dolor amet consetetur adi pisicing elit sed eiusm tempor in cididunt ut labore dolore magna aliqua enim.</p>
+      <div className="footer-top pt-130">
+      <div className="vector-bg" style={{...back}}></div>
+                <div className="auto-container">
+                    <div className="row clearfix">
+                        <div className="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div className="footer-widget logo-widget">
+                                <figure className="footer-logo"><img src="https://res.cloudinary.com/djqty7gls/image/upload/v1670449285/logo-new_lco1yc.png" alt="" style={{...width}}></img></figure>
+                                <div className="text">
+                                    <p>At go shop, our purpose is to enrich lives through the latest products. We do that by leveraging our unique combination of tech expertise and human touch to meet our customers’ everyday needs.</p>
                                 </div>
-                                <ul class="social-links clearfix">
-                                    <li><a href='https://www.facebook.com/goshoppingonline1' target='_blank'><i class="fa fa-facebook-f"></i></a></li>
-                                    <li><i class="fa fa-twitter"></i></li>
-                                    <li><a href='https://www.instagram.com/goshop.nsk/' target='_blank'><i class="fa fa-instagram"></i></a></li>
+                                <ul className="social-links clearfix">
+                                    <li><a href='https://www.facebook.com/goshoppingonline1' target='_blank' ><i className="fa fa-facebook-f"></i></a></li>
+                                    <li><i className="fa fa-twitter"></i></li>
+                                    <li><a href='https://www.instagram.com/goshop.nsk/' target='_blank'><i className="fa fa-instagram"></i></a></li>
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                            <div class="footer-widget links-widget">
-                                <div class="widget-title">
+                        <div className="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div className="footer-widget links-widget">
+                                <div className="widget-title">
                                     <h3>Services</h3>
                                 </div>
-                                <div class="widget-content">
-                                    <ul class="links-list clearfix">
+                                <div className="widget-content">
+                                    <ul className="links-list clearfix">
                                         <li>About Us</li>
-                                        <li>Listing</li>
-                                        <li>How It Works</li>
                                         <li>Our Services</li>
                                         <li>Our Blog</li>
                                         <li>Contact Us</li>
@@ -44,16 +42,16 @@ const Footer = () => {
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                            <div class="footer-widget contact-widget">
-                                <div class="widget-title">
+                        <div className="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div className="footer-widget contact-widget">
+                                <div className="widget-title">
                                     <h3>Contacts</h3>
                                 </div>
-                                <div class="widget-content">
-                                    <ul class="info-list clearfix">
-                                        <li><i class="fa fa-map-marker-alt"></i>Flat 20, Reynolds Neck, North Helenaville, FV77 8WS</li>
-                                        <li><i class="fa fa-microphone"></i><a href="tel:23055873407">+1(305) 587-3407</a></li>
-                                        <li><i class="fa fa-envelope"></i><a href="mailto:info@example.com">info@goshop.com</a></li>
+                                <div className="widget-content">
+                                    <ul className="info-list clearfix">
+                                        <li><i className="fa fa-map-marker-alt"></i>Flat 20, Reynolds Neck, North Helenaville, FV77 8WS</li>
+                                        <li><i className="fa fa-microphone"></i><a href="tel:23055873407">+1(305) 587-3407</a></li>
+                                        <li><i className="fa fa-envelope"></i><a href="mailto:info@example.com">info@goshop.com</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -61,13 +59,13 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div class="footer-bottom">
-                <div class="auto-container">
-                    <div class="bottom-inner clearfix">
-                        <div class="copyright pull-left">
+            <div className="footer-bottom">
+                <div className="auto-container">
+                    <div className="bottom-inner clearfix">
+                        <div className="copyright pull-left">
                             <p><a href="index.html">Go Shop</a> &copy; 2022 All Right Reserved</p>
                         </div>
-                        <ul class="footer-nav pull-right">
+                        <ul className="footer-nav pull-right">
                             <li><a href="index.html">Terms of Service</a></li>
                             <li><a href="index.html">Privacy Policy</a></li>
                         </ul>
